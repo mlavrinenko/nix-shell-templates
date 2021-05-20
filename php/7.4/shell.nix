c2fe302165148d3bfe3d9f960809734df34427c3
Maxim Lavrinenko <maxim@lavrinenko.xyz>
2021-05-20 23:15:34 +0300

    feat(php): add php 7.4 environment with optional xdebug

---
+++ php/7.4/shell.nix
@@ -0,0 +1,15 @@
+# Regular shell
+# $ nix-shell
+# Shell with xdebug enabled
+# $ nix-shell --arg debug true
+
+{ debug ? false, pkgs ? import <nixpkgs> {} }:
+with pkgs;
+let php = php74.withExtensions ({ all, enabled }: with all; [ opcache json xdebug ] ++ enabled);
+in mkShell {
+    buildInputs = [ php php.packages.composer nodejs-14_x ];
+    XDEBUG_MODE = "debug";
+    XDEBUG_CONFIG = lib.optional debug "idekey=PHPSTORM remote_enable=1";
+    PHP_IDE_CONFIG = "serverName=localhost";
+    LOCALE_ARCHIVE="${pkgs.glibcLocales}/lib/locale/locale-archive";
+}
